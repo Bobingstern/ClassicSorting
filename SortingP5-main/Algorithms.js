9b91a0f40b20d0fc8a40c6124879ff0f98030a4f
Bob Boi <74193405+Bobingstern@users.noreply.github.com>
2021-05-04 14:20:54 -0400

    Update Algorithms.js

--- SortingP5-main/Algorithms.js
+++ SortingP5-main/Algorithms.js
@@ -276,9 +276,52 @@ async function insertionSortRecursive(arr, n)
 }
 //---
 
-//Bitonic Hell
+//Heap sort
 
+async function heapify(arr, n, i){
 
+    let largest = i; // Initialize largest as root
+    let l = 2 * i + 1; // left = 2*i + 1
+    let r = 2 * i + 2; // right = 2*i + 2
+ 
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest]){
+        largest = l;
+    }    
+ 
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest]){
+        largest = r;
+    }
+ 
+    // If largest is not root
+    if (largest != i) {
+        await swap(arr, i, largest);
+        states[i] = 0
+ 
+        // Recursively heapify the affected sub-tree
+        await heapify(arr, n, largest);
+    }
+}
+
+async function heapSort(arr, n)
+{
+    // Build heap (rearrange array)
+    for (let i = n / 2 - 1; i >= 0; i--){
+        await heapify(arr, n, i);
+
+      }
+ 
+    // One by one extract an element from heap
+    for (let i = n - 1; i > 0; i--) {
+        // Move current root to end
+        await swap(arr, 0, i);
+
+ 
+        // call max heapify on the reduced heap
+        await heapify(arr, i, 0);
+    }
+}
 
 ///-----
 
